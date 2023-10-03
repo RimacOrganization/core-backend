2b29f4ecec000368c943ebe31fa7cf7d62d1ad4c
mavalentin <mavalentin@vectoritcgroup.com>
2023-10-03 11:29:42 -0500

    MC-7 add login

---
+++ login.py
@@ -0,0 +1,17 @@
+
+class Login:
+
+    def initial_session(self):
+        try:
+            print("Iniciando sesión ...")
+        except Exception as e:
+            print(e)
+
+    def close_session(self):
+        try:
+            print("Cerrando sesión ...")
+        except Exception as e:
+            print(e)            
+
+Login().initial_session()
+Login().close_session()
