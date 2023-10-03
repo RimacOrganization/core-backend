9edc097ab982ae58e4e3cb95a1bc05a90085adfa
mavalentin <mavalentin@vectoritcgroup.com>
2023-10-03 15:45:17 -0500

    add calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+
+class Calculator:
+
+    def sum(self,a,b):
+        result = a + b
+        print(f"La suma de {a} + {b} = {result}")
+
+    def diff(self,a,b):
+        result = a - b
+        print(f"La suma de {a} - {b} = {result}")
+
+Calculator().sum(10,20)
+Calculator().diff(100,20)
--- saludo.py
+++
@@ -1,8 +0,0 @@
-
-class Saludo:
-
-    def sum(self,a,b):
-        result = a + b
-        print(f"La suma de {a} + {b} = {result}")
-
-Saludo().sum(10,20)
