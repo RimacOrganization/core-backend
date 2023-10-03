885c62d20c5d43678b084d269bce00b305886b45
mavalentin <mavalentin@vectoritcgroup.com>
2023-10-03 15:33:50 -0500

    MC-2 add end process

--- process_backend.py
+++ process_backend.py
@@ -4,8 +4,16 @@ class ProcessBackEnd:
     def init_process(self):
         try:
             for i in range (0,100,10):
-                print(f"process {i}")
+                print(f"Init process {i}")                
         except Exception as e:
             print (e)
 
-ProcessBackEnd().init_process()            
+    def finish_process(self):
+        try:
+            for i in range (0,100,10):
+                print(f"End process {i}")                
+        except Exception as e:
+            print (e)            
+
+ProcessBackEnd().init_process()
+ProcessBackEnd().finish_process()
